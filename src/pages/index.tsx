1a77812a0aaa1d89c4ef209335d474e1446aaa45
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-02-17 14:12:36 -0600

    Add tailwind integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,19 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Vicente Gutierréz</title>
+        <title>Vicente Gutiérrez</title>
         <meta name="description" content="Vicente Gutiérrez Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <p>Hello!</p>
+        <h1 className='text-4xl font-extrabold'>
+          Hello&nbsp;
+          <span className="text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-blue-500">
+            World!
+          </span>
+        </h1>
+        
       </main>
     </>
   )
