3ef8026ac7e97caaef708949747dd0cc74d008b9
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-02-17 15:56:18 -0600

    Enable semicolon rule

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -18,5 +18,5 @@ export default function Home() {
         </h1>
       </main>
     </>
-  )
+  );
 }
