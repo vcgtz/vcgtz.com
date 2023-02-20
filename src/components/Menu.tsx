668a5df8aace0073bb437718e9e3597016d8e197
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-02-20 11:31:51 -0600

    Show active menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,36 +1,56 @@
 import MenuItem from '@/components/MenuItem';
+import { NextRouter, useRouter } from 'next/router';
 
 type MenuListItem = {
   name: string;
   link: string;
+  enabled: boolean;
 };
 
 const menuList: MenuListItem[] = [
   {
     name: 'Home',
-    link: '#',
+    link: '/',
+    enabled: true,
   },
   {
     name: 'Projectos',
     link: '#',
+    enabled: false,
   },
   {
     name: 'Blog',
     link: '#',
+    enabled: false,
+  },
+  {
+    name: 'Contacto',
+    link: '#',
+    enabled: false,
+  },
+  {
+    name: 'Mi Setup',
+    link: '#',
+    enabled: false,
   },
 ];
 
 export default function Menu() {
+  const router: NextRouter = useRouter();
+
   return (
     <>
       {
-        menuList.map((item) => (
-          <MenuItem
-            key={ item.name }
-            name={ item.name }
-            link={ item.link }
-          />
-        ))
+        menuList.filter((item) => item.enabled)
+          .map((item) => (
+            <MenuItem
+              key={ item.name }
+              name={ item.name }
+              link={ item.link }
+              isCurrentPath={ router.pathname === item.link }
+            />
+          )
+        )
       }
     </>
   );
