2b2d3d51fe4520a6c5d7dd13d55c0d211ae22d30
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-02-20 15:21:01 -0600

    Add PHP to the main description

--- src/components/MainDescription.tsx
+++ src/components/MainDescription.tsx
@@ -5,7 +5,7 @@ export default function MainDescription() {
         Qué tal! Me llamo Vicente y soy <span className='font-bold'>Ingeniero de Software</span>.
       </p>
       <p className='text-lg mb-6 font-light text-gray-600'>
-        Estoy enfocado en tecnologías como <span className='font-medium'>JavaScript</span>, <span className='font-medium'>TypeScript</span> & <span className='font-medium'>Python</span> principalmente como backend, pero no exclusivamente.
+        Estoy enfocado en tecnologías como <span className='font-medium'>JavaScript</span>, <span className='font-medium'>TypeScript</span>, <span className='font-medium'>Python</span> & <span className='font-medium'>PHP</span> principalmente como backend, pero no exclusivamente.
       </p>
       <p className='text-lg mb-6 font-light text-gray-600'>
         Me apasiona programar y crear nuevas aplicaciones. Me encanta aprender nuevas tecnologías, probarlas y ver que cosas se pueden desarrollar con ellas. Todos mis proyectos son Open Source, me gusta el código limpio y simple, me interesa el diseño de sistemas y actualmente estoy introduciéndome al desarrollo de videojuegos como hobby.
