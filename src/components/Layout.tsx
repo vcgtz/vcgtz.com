f92c6f538fdcfd0df93eb471d063cbcedaf924e5
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-02-20 17:23:23 -0600

    Move meta tags to the main layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
@@ -9,6 +10,13 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Head>
+        <title>Vicente Gutiérrez</title>
+        <meta name="description" content="Portafolio de proyectos de Vicente Gutiérrez" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-5xl">
           <Header />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
-import Head from 'next/head';
 import MainPicture from '@/components/MainPicture';
 import MainDescription from '@/components/MainDescription';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Vicente Gutiérrez</title>
-        <meta name="description" content="Vicente Gutiérrez Website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
+    <> 
       <main>
         <div className="px-6 py-6 md:py-16">
           <MainPicture />
