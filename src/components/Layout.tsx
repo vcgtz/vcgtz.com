de4c10236106944771b230bbc441542eebb9aa53
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-02-20 18:20:26 -0600

    Make public the GA id

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -20,7 +20,7 @@ export default function Layout({ children }: LayoutProps) {
 
       {/*  Global site tag (gtag.js) - Google Analytics */}
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -29,7 +29,7 @@ export default function Layout({ children }: LayoutProps) {
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
         `}
       </Script>
 
