d484c9b5282097ea92a5cee350be861bf0ef1468
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-04-29 22:23:59 -0600

    Fix image position in mobile view

--- src/components/MainPicture.tsx
+++ src/components/MainPicture.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function MainPicture() {
   return (
     <Image
-      className="mx-auto mb-4 h-48 w-48 rounded-lg -rotate-4 md:float-left md:mr-8 md:mb-2"
+      className="mx-auto mb-4 h-48 w-48 rounded-lg md:-rotate-4 md:float-left md:mr-8 md:mb-2"
       src="/assets/images/vicente-gutierrez.jpg"
       alt="Vicente Gutiérrez"
       width={200}
